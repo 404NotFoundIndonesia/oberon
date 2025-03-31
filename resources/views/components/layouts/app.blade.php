1a74ef010494c5c9819580c8fe5ade7d2885bf29
Andika Sujanadi <andikasujanadi@gmail.com>
2025-03-31 15:48:03 +0800

    integratting tailwind in livewire template

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,10 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <link rel="stylesheet" href="">
+        @vite('resources/css/app.css')
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
     <body>
+        <h1 class="text-3xl font-bold underline text-red-500">
+            Hello world!
+          </h1>
         {{ $slot }}
     </body>
 </html>
