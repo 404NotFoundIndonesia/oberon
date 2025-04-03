70c905aca29e61d6b0deaf087866d5687c24a929
Andika Sujanadi <andikasujanadi@gmail.com>
2025-04-03 09:37:58 +0800

    create login screen

---
+++ resources/views/livewire/page/login.blade.php
@@ -0,0 +1,24 @@
+<div class="flex flex-col sm:flex-row items-center h-full max-w-4xl mx-auto">
+    <div class="sm:w-1/2 h-full mt-auto p-4 sm:pt-18">
+        <div class="w-full h-full bg-blue-600 rounded-4xl text-white p-12">
+            <div class="font-medium">OBERON</div>
+            <div class="sm:mt-20 text-4xl mb-4">
+                Kelola koperasi dengan mudah, transparan, dan efisien.
+            </div>
+            <div class="sm:mb-12">
+                Oberon menyediakan fitur lengkap untuk pengelolaan anggota, transaksi, simpanan, pinjaman, dan laporan keuangan.
+            </div>
+        </div>
+    </div>
+    <form class="flex flex-col gap-1 pt-0 sm:pt-8 p-8 w-full sm:w-2/3 me-auto">
+        <div class="font-medium text-2xl">Masuk Untuk Melanjutkan</div>
+        <div class="text-sm">Username</div>
+        <input class="form-input" name="username" autocomplete="off" type="text">
+        <div class="text-sm">Password</div>
+        <input class="form-input" name="password" type="password">
+        <div class="btn me-auto flex  mt-3">
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-send"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 14l11 -11" /><path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" /></svg>
+            <div>Masuk</div>
+        </div>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 use App\Livewire\Page\Landing;
+use App\Livewire\Page\Login;
+use App\Livewire\Page\Admin;
 
 Route::get('/', Landing::class);
 Route::get('/about', Landing::class);
 Route::get('/contact', Counter::class);
+Route::get('/login', Login::class);
+Route::get('/admin', Admin::class);
